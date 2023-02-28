08c51e71965ae7f278de548ad8b7462999320bd8
=Rui Carlos <ruicarlos3g.rc@gmail.com>
2023-02-28 09:32:16 -0300

    Inicial

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import Task from "./src/pages/Task";
+import NewTask from "./src/pages/NewTask";
+import Details from "./src/pages/Details";
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Task">
+        <Stack.Screen
+          name="Task"
+          component={Task}
+          options={{
+            headerTintColor:"#f92e6a"
+          }}
+        />
+
+        <Stack.Screen
+          name="New Task"
+          component={NewTask}
+          options={{
+            headerTintColor:"#704F59"
+          }}
+        />
+
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            headerTintColor:"#26643E"
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
